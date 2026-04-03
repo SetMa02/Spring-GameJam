acb62198764ea562edc09b36b9065b929785a952
Mojgas <olegnaumovoff@gmail.com>
2026-04-03 22:19:09 +0500

    Spike Obsticle

---
+++ SpringGameJam/Assets/Scripts/Obsticles/SpikeObsticle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeObsticle : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.Die();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.Die();
+        }
+    }
+    
+}
