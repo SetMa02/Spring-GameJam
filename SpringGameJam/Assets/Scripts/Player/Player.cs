e0cc4e09a9fb14649195c233423859b3cc934fd5
Максим Майнагашев <maxuser99@mail.ru>
2026-04-03 19:09:18 +0300

    Acceleration and movment improved

--- SpringGameJam/Assets/Scripts/Player/Player.cs
+++ SpringGameJam/Assets/Scripts/Player/Player.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +6,20 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    [SerializeField]private float _jumpForce;
+    [SerializeField] private float _maxSpeed = 8f;
+    [SerializeField] private float _acceleration = 50f;
+    [SerializeField] private float _deceleration = 40f;
+    [SerializeField] private float _airControlPercent = 0.5f;
+    
+    [SerializeField] private float _jumpForce = 15f;
 
-    private bool _platformDetector;
-    private Vector3 _direction;
     private Rigidbody2D _rb;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+
+    private bool _isGrounded = false; 
+    private float _horizontalInput;
+    private int _platformsContactCount = 0; 
 
     private void Start()
     {
@@ -25,49 +30,83 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Movement();
-        StartJump();
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        }
     }
 
-    private void Movement()
+    private void FixedUpdate()
     {
-        _direction = Vector3.zero;
+        MoveCharacter(_horizontalInput);
+        UpdateAnimations(_horizontalInput);
+    }
 
-        if (Input.GetKey(KeyCode.A))
+    private void MoveCharacter(float horizontal)
+    {
+        float actualAcceleration = _isGrounded ? _acceleration : _acceleration * _airControlPercent;
+        float actualDeceleration = _isGrounded ? _deceleration : _deceleration * _airControlPercent;
+
+        if (horizontal != 0)
         {
-            _direction = Vector3.left;
+            _rb.AddForce(new Vector2(horizontal * actualAcceleration, 0));
+            
+            if (Mathf.Abs(_rb.velocity.x) > _maxSpeed)
+            {
+                _rb.velocity = new Vector2(_maxSpeed * Mathf.Sign(_rb.velocity.x), _rb.velocity.y);
+            }
         }
-        else if (Input.GetKey(KeyCode.D))
+        else
         {
-            _direction = Vector3.right;
+            if (Mathf.Abs(_rb.velocity.x) > 0.01f)
+            {
+                _rb.AddForce(new Vector2(-_rb.velocity.x * actualDeceleration, 0));
+            }
+            else if (Mathf.Abs(_rb.velocity.x) < 0.01f)
+            {
+                _rb.velocity = new Vector2(0, _rb.velocity.y);
+            }
         }
-        
-        _direction *= _speed;
-        _direction.y = _rb.velocity.y;
-        _rb.velocity = _direction;
+    }
+
+    private void UpdateAnimations(float horizontal)
+    {
+        if (horizontal > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (horizontal < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+
     }
     
-    private void StartJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && _platformDetector == true)
-        {
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.TryGetComponent(out Platforms platform))
+        if (other.gameObject.GetComponent<Platforms>() != null)
         {
-            _platformDetector = true;
+            _platformsContactCount++;
+            if (_platformsContactCount > 0)
+            {
+                _isGrounded = true;
+            }
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (other.gameObject.TryGetComponent(out Platforms platform))
+        if (other.gameObject.GetComponent<Platforms>() != null)
         {
-            _platformDetector = false;
+            _platformsContactCount--;
+            if (_platformsContactCount <= 0)
+            {
+                _isGrounded = false;
+                _platformsContactCount = 0;
+            }
         }
     }
 }
