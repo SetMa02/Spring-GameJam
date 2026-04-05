7fa88e739a727daf35a7673aeffc7c44b7ba8063
Максим Майнагашев <maxuser99@mail.ru>
2026-04-05 15:46:37 +0300

    Bug Fixed

--- SpringGameJam/Assets/Scripts/Obsticles/SawObsticle.cs
+++ SpringGameJam/Assets/Scripts/Obsticles/SawObsticle.cs
@@ -3,23 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SawObsticle : MonoBehaviour
-
 {
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Player player = collision.gameObject.GetComponent<Player>();
-
-        if (player != null)
-        {
-            player.Die();
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Player player = collision.GetComponent<Player>();
-
-        if (player != null)
+        if (collision.gameObject.TryGetComponent(out Player player))
         {
             player.Die();
         }
